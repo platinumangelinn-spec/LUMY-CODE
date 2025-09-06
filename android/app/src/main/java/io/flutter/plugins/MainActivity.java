32521cdc21ba8e34de20a8b6237f78a3538fd3fa
platinumangelinn-spec <platinum.angelinn@gmail.com>
2025-09-06 17:40:59 -0600

    Delete android/app/src/main/java/io/flutter/plugins/MainActivity.java

--- android/app/src/main/java/io/flutter/plugins/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.lumy; // ⚠️ cambia esto si tu paquete tiene otro nombre
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
